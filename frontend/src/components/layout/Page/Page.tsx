f755c3399f530486bfe35f7b74f923467224219c
Seb-Prod <sebastien.drillaud@gmail.com>
2025-10-14 15:26:15 +0200

    ajout du footer

--- frontend/src/components/layout/Page/Page.tsx
+++ frontend/src/components/layout/Page/Page.tsx
@@ -1,5 +1,6 @@
 import type { JSX, ReactNode } from "react";
 import styles from "./Page.module.css";
+import Footer from "../Footer";
 
 
 type PageProps = {
@@ -12,6 +13,7 @@ const Page = ({ children }: PageProps): JSX.Element => {
         <div className={styles.main}>
             {children}
         </div>
+        <Footer/>
        </div>
     );
 }
