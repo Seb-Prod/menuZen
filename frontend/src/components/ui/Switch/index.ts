7f39c06e07097eeee2448cda6613286ddbbcd91d
Seb-Prod <sebastien.drillaud@gmail.com>
2025-11-07 19:31:31 +0100

    doc index

--- frontend/src/components/ui/Switch/index.ts
+++ frontend/src/components/ui/Switch/index.ts
@@ -1,1 +1,11 @@
-export { default } from "./Switch";
+/**
+ * @file Point d'entrée pour le composant Switch
+ * @module components/ui/Switch
+ * @version 1.0.0
+ * @since 2025-11-07
+ * @author Seb-Prod
+ */
+
+export { default } from './Switch';
+export type { Props as SwitchProps } from './Switch.types';
+
